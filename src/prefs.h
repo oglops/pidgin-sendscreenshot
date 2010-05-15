5495a2587da18b7eb0e0e98ba4edabd4d41f5530
raoul.berger@yahoo.fr <raoul.berger@yahoo.fr@6fee7fa2-2a33-11df-9816-a3eb4afb4c33>
2010-05-15 13:46:47 +0000

    Added option "immediately return after mouse release". Default is checked. If Unchecked, let the user modify the selection. Press enter to finalize the selection.

--- src/prefs.h
+++ src/prefs.h
@@ -45,6 +45,7 @@
 #define PREF_ONLY_SAVE_WHEN PREF_PREFIX "/only-save-when"
 #define PREF_SHOW_VISUAL_CUES PREF_PREFIX "/show_visualcues"
 #define PREF_WAIT_BEFORE_SCREENSHOT PREF_PREFIX "/wait_time"
+#define PREF_RETURN_IMMEDIATELY PREF_PREFIX "return-immediately"
 #define PREF_ADD_SIGNATURE PREF_PREFIX "/add-signature"
 #define PREF_SIGNATURE_FILENAME PREF_PREFIX "/signature-filename"
 
@@ -99,7 +100,9 @@
 #define PREF_UI_ONLY_SAVE_WHEN _("Only save when sending as a File.")
 
 #define PREF_UI_SHOW_VISUAL_CUES _("Show visual cues.")
+
 #define PREF_UI_WAIT_BEFORE_SCREENSHOT _("Seconds to wait before desktop freezes:")
+#define PREF_UI_RETURN_IMMEDIATELY _("Immediately return after mouse release.")
 
 #ifdef ENABLE_UPLOAD
 #define TAB_1 _("General")
